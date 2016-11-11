9bd7f1d7505cb0a17daf1e7ac8adb5370493be39
rdvikki <wr.viggu@gmail.com>
2016-11-11 11:07:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article1', function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article2', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article2', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
