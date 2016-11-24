4b23e461527ab481abf0d59ea1517ac36d42abf4
rdvikki <wr.viggu@gmail.com>
2016-11-24 17:46:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='Hi, This is Vigneshwari';
+
 var img=document.getElementById('madi');
 
-   marginLeft=0;
+ var marginLeft=0;
    function moveRight(){
        marginLeft=marginLeft+10;
        img.style.marginLeft = marginLeft + 'px';
    }
    
    img.onclick = function () {
-    var interval=setInterval(moveRight,100);
+    var interval = setInterval(moveRight,100);
     };
