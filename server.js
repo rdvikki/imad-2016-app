3c324ecc56b879fa83e0a2e1462ecece7634c98d
rdvikki <wr.viggu@gmail.com>
2016-11-25 22:07:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var pool = require('pg').Pool;
 
 var config = {
-    host:'http://rdvikki.imad.hasura-app.io/',
+    host:'db.imad.hasura-app.io/',
     user:'rdvikki',
     database:'rdvikki',
     port:'5432',
