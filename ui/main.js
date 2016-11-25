5bf58552fd6c875275407f07ed0872a2c4e0eec6
rdvikki <wr.viggu@gmail.com>
2016-11-25 10:58:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
 console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='Hi, This is Vigneshwari';
-
-var img=document.getElementById('madi');
-
- var marginLeft=0;
-   function moveRight(){
-       marginLeft=marginLeft+1;
-       img.style.marginLeft = marginLeft + 'px';
-   }
-   
-   img.onclick = function () {
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter=0;
+//creating a request object
+button.onclick=function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.Done && request.status===200){
+           {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
     };
+    //making a request
+    
+    request.open('Get','http://rdvikki.imad.hasura-app.io',true);
+   request.send(null);
+    
+    
+};
