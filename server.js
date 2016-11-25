b72c72239abec47e96ba711058f13e433e715d99
rdvikki <wr.viggu@gmail.com>
2016-11-25 12:57:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names=[];
+app.get('/submit-name/:name', function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+    
+});
+
 app.get('/:articleName', function (req, res){
     articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -101,13 +110,8 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/submit-name/:name', function(req,res){
-    var name = req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-    
-});
+
+
 
 
 
