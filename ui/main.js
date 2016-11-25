be5445bf8ff201b1a6ac1f24459e47f7df5e9147
rdvikki <wr.viggu@gmail.com>
2016-11-25 13:21:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,15 +20,29 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
+    
+         var request = new XMLHttpRequest();
+         
+                request.onreadystatechange=function(){
+                    if(request.readyState === XMLHttpRequest.DONE && request.status===200){
+                     var names=request.responseText;
+                     names=JSON.parse(names);
+                     var list='';
+                     for (v=0; i< names.length; i++)
+                     {
+                         list+= '<li>' + name[i] + '</li>';
+                     }
+                    
+                        var ul=document.getElementById('namelist');
+                        ul.innerHTML = list; 
+                                        
+                      }
+                
+                };
+                request.open('Get','http://rdvikki.imad.hasura-app.io/submit-name?name=' + name,true);
+                request.send(null);
+
  //make a request to the server and send the names
  //capture a list of name and render it as list
- var names=['name1','name2','name3'];
- var list='';
- for (v=0; i< names.length; i++)
- {
-     list+= '<li>' + name[i] + '</li>';
- }
 
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
 };
