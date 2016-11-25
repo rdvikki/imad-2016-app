a94ce72b3e162c9aa6e01c5352c88e2cd4f7c5f6
rdvikki <wr.viggu@gmail.com>
2016-11-25 13:24:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onclick = function () {
             };
    
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit-btn');
 submit.onclick = function(){
     
@@ -39,6 +38,8 @@ submit.onclick = function(){
                       }
                 
                 };
+                var nameInput = document.getElementById('name');
+                var name = nameInput.value;
                 request.open('Get','http://rdvikki.imad.hasura-app.io/submit-name?name=' + name,true);
                 request.send(null);
 
