f8df9a59fa63a9ed323b79e8511b776527a487ea
rdvikki <wr.viggu@gmail.com>
2016-11-26 10:07:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/:articleName', function (req, res){
     articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-//app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/main.js', function (req, res) {
