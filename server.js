e4d9ade31bd445b4d0105e1ada9d1fb4caafe812
rdvikki <wr.viggu@gmail.com>
2016-11-26 17:07:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,17 +85,17 @@ app.get('/', function (req, res) {
 
 //create the pool somewhere globally so its lifetime
 // lasts for as long as your app is running
-//var pool = new Pool(config); 
-//app.get('/test-db', function(req,res){
-//pool.query('SELECT * FROM test', function (err,result){
-//if(err){
-//  res.status(500).send(err.toString());
-//}
-//    else{
-//        res.send(JSON.stringify(result.rows));
-//}
-//});
-//});
+var pool = new Pool(config); 
+app.get('/test-db', function(req,res){
+pool.query('SELECT * FROM test', function (err,result){
+if(err){
+  res.status(500).send(err.toString());
+}
+    else{
+        res.send(JSON.stringify(result.rows));
+}
+});
+});
 var counter=0;
 app.get('/counter', function(req,res)
 {
