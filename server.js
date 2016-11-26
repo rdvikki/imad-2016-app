21022fad2db9892b7262fff2fa09d97e4b5e5e09
rdvikki <wr.viggu@gmail.com>
2016-11-26 10:07:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-//var Pool = require('pg').Pool;
+var Pool = require('pg').Pool;
 
-//var config = {
+var config = {
     user:'rdvikki',
     database:'rdvikki',
     host:'db.imad.hasura-app.io/',
